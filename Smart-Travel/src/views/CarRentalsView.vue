a7b190d65d63aec8dddeaa10fc4294e5fe1f7fae
fhashmi <eu6179@wayne.edu>
2022-10-05 13:23:03 -0400

    Car Rentals Frontend Formatted

--- Smart-Travel/src/views/CarRentalsView.vue
+++ Smart-Travel/src/views/CarRentalsView.vue
@@ -1,22 +1,23 @@
 
 <template>
-<div class="bg-white">
-<div class="mx-auto max-w-4xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
 
-  <form action="#" class="text-center ">
+<div class=" mt-6 mx-auto max-w-4xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+
+  <form action="#" class="text-center flex">
     <div>
-      <div class="space-y-10 md:grid md:grid-cols-3 md:gap-3 md:space-y-0 lg:grid-cols-3">
-        <div><input class = "block mb-2 text-sm rounded-lg font-medium text-gray-900 dark:text-gray-400"
-                    type="text" v-model="searchLocation" placeholder="Pickup Location"/> </div>
-        <Datepicker input placeholder="Pickup Date">> </Datepicker>
-        <Datepicker input placeholder="Return Date">> </Datepicker>
-
+      <div class=" md:grid py-15 md:grid-cols-3 md:gap-3 md:space-y-0">
+        <input class = "block mb-2 rounded-lg text-sm font-medium text-gray-900 dark:text-gray-400"
+                    type="text" v-model="searchLocation" placeholder="Pickup Location"/>
+        <Datepicker input placeholder="Pickup Date" class=" block mb-2 text-sm rounded-lg font-medium text-gray-900 dark:text-gray-400"> </Datepicker>
+        <Datepicker input placeholder="Return Date" class = " block mb-2 text-sm rounded-lg font-medium text-gray-900 dark:text-gray-400"> </Datepicker>
         <div v-for="pickup in locationpickup" :key="pickup.id"> {{ pickup.label }}</div>
       </div>
+      <button @click="search" value="Search for Vehicle" class= "mt-6 w-1/2 items-center py-2  p-2.5 rounded-md border border-transparent bg-indigo-900 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 text-center"> Search </button>
     </div>
-    <button @click="search" value="Search for Vehicle" class="flex-auto w-center justify-center rounded-md border border-transparent bg-indigo-900 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700  " > Search </button>
   </form>
+  <h3 class="text-xl font-medium text-gray-900 mt-6 " > Top Cities to Visit</h3>
 </div>
+
 <footer class="mx-auto w-full  bg-white mt-auto" aria-labelledby="footer-heading">
   <div class="mx-auto  py-15 px-4 sm:px-6 lg:py-20 lg:px-8">
     <div class="mt-12 border-t border-gray-200 pt-8">
@@ -30,7 +31,6 @@
     </div>
   </div>
 </footer>
-  </div>
 </template>
 <script>
 import Datepicker from "@vuepic/vue-datepicker";
