25f5d7b9117797ba2fbd315f688f03408d8b2575
Yolanda M <91237202+ymason48@users.noreply.github.com>
2022-10-07 14:53:13 -0400

    I added javascript to the flights page and change the body of the document in all sections.

--- FlightsView.vue
+++ FlightsView.vue
@@ -5,32 +5,36 @@
       <div class="mt-10 space-y-10 ">
         <form action="#" method="POST" class="space-y-10 ">
           <div class="sm:col-span-2">
-            <label for="city" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Leaving from</label>
+            <label for="leavingFromId" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Leaving from</label>
             <div class="mt-1">
-              <input type="text" name="city" id="city" autocomplete="city" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+              <input type="leavingFromId" name="leavingFromId" id="leavingFromId" autocomplete="leavingFromId" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </div>
             </div>
 
 
           <div class="sm:col-span-2">
-            <label for="city" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Going to</label>
+            <label for="goingToId" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Going to</label>
             <div class="mt-1">
-              <input type="text" name="city" id="city" autocomplete="city" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+              <input type="goingToId" name="goingToId" id="goingToId" autocomplete="goingToId" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </div>
             </div>
 
 
            <div class="sm:col-span-">
-		         <label for = "dates" class = "block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"> Dates </label>
+		         <label for = "departDate" class = "block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"> Departure date </label>
 		         <div class="mt-1">
-		        <input type = "date" id = "dates" name= "dates" autocomplete="dates" required="" class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"  />
+		        <input type = "date" id = "departDate" name= "departDate" autocomplete="departDate" required="" class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"  />
+	          </div>
+             <label for = "returnDate" class = "block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"> Return date </label>
+		         <div class="mt-1">
+		        <input type = "date" id = "returnDate" name= "returnDate" autocomplete="returnDate" required="" class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"  />
 	          </div>
 	        </div>
 	   
 
 
           <div class="sm:col-span-2">
-            <label for="person" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Travelers </label>
+            <label for="travelersId" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Travelers </label>
              <select id="number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option> -- </option>
                 <option>1 adult 0 children</option>
@@ -47,9 +51,8 @@
 
 
           <div class="sm:col-span-2">
-            <label for="number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Cabin class</label>
+            <label for="cabinClassId" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Cabin class</label>
             <div class="mt-1">
-              <label for="seat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Cabin class</label>
                <select> id="seat" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option> -- </option>
                 <option>First class</option>
@@ -66,7 +69,7 @@
         </form>
         <br>
 
-        <h3> TOP CITIES TO VISIT </h3>
+        <h3> <class="text-xl font-medium text-gray-900 mt-6 " > TOP CITIES TO VISIT </h3>
       </div>
 
     </div>
@@ -90,9 +93,30 @@
 </template>
 
 
+<script>
+import departDate from "@vuepic/vue-departDate";
+import returnDate from "@vuepic/vue-returnDate";
+import travelersId from "@vuepic/vue-travelersId";
+import cabinClassId from "@vuepic/vue-cabinClassId";
 
-
-
-
-
-
+export default {
+  components: {departDate},
+  components: {returnDate},
+  components: {travelersId},
+  components: {cabinClassId},
+  name: 'Flights',
+  data() {
+    return{
+      apiKey: '<cc68175e3amsh222151b1246494ap126e49jsnb4e35549a93b>',
+      leavingFromId: '',
+      goingToId: '',
+      departDate:'date' ,
+      returnDate'date',
+      travelersId: '',
+      cabinClassId: '',
+    }
+  },
+  methods:{
+  }
+}
+</script>
