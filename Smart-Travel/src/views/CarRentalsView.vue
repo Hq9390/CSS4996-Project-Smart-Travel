f2e3f383e57e1b70e09d7a353806233bd64fb5e4
fhashmi <eu6179@wayne.edu>
2022-10-26 14:13:08 -0400

    Car Rentals Page Populated with API Data

--- Smart-Travel/src/views/CarRentalsView.vue
+++ Smart-Travel/src/views/CarRentalsView.vue
@@ -1,82 +1,129 @@
 <template>
 
-  <div class=" mt-6 mx-auto max-w-5xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+  <div class=" mt-6 mx-auto max-w-6xl py-16 px-4 sm:py-20 sm:px-6 lg:px-8 ">
 
-    <form action="#" class="text-center flex">
-      <div>
-        <div class=" md:grid py-15 md:grid-cols-6 md:gap-3 md:space-y-0">
+    <form action="#" class="align-items-center bg-light mb-3 ">
+      <div class="mt-6">
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2  md:grid-cols-4 lg:grid-cols-6">
+
+      <div class="flex flex-1 flex-col ">
           <input v-model = "location"
                  @input="getLocationResult"
-                 class = "block mb-2 rounded-lg text-sm font-medium text-gray-900 dark:text-gray-400"
+                 class = "col-span-1 block rounded-lg text-sm font-medium text-gray-900 dark:text-gray-400"
                  type ="text"
                  placeholder="Pickup Location"
                  name="location"
                  id="location"
           />
+      </div>
 
-          <div>
-            <Datepicker  v-model="pickupDate"
-                         placeholder="Pickup Date"
-                         autoApply :format="format"
-                         :min-date='new Date()'
-                         class=" block mb-2 text-sm rounded-lg font-medium text-gray-900 dark:text-gray-400">
-            </Datepicker>
+
+          <div class="flex flex-1 flex-col ">
+          <Datepicker  v-model="pickupDate"
+                       placeholder="Pickup Date"
+                       autoApply :format="format"
+                       :min-date='new Date()'
+                       class=" col-span-1 block mb-2 text-sm rounded-lg font-medium text-gray-900 dark:text-gray-400">
+
+          </Datepicker>
           </div>
-          <div>
+
+          <div class="flex flex-1 flex-col ">
+          <input v-model = "pickupTime"
+                 @input="getpickupTime"
+                 class = " col-span-1 block mb-2 rounded-lg text-sm font-medium text-gray-900 dark:text-gray-400"
+                 type = 'time'
+                 placeholder="Pickup Time"
+                 name="pickupTime"
+                 id="pickupTime"
+          />
+            <label for="pickupTime" class=" text-center lock text-sm font-medium text-gray-700">Pickup Time</label>
+        </div>
+
+          <div class="flex flex-1 flex-col ">
             <Datepicker  v-model= "returnDate"
                          placeholder="Return Date"
-                         class = " block mb-2 text-sm rounded-lg font-medium text-gray-900 dark:text-gray-400"
+                         class = " col-span-1 block mb-2 text-sm rounded-lg font-medium text-gray-900 dark:text-gray-400"
                          autoApply :format ="format"
                          :min-date='new Date()'>
             </Datepicker>
           </div>
-          <div>
-            <input v-model = "pickupTime"
-                   @input="getpickupTime"
-                   class = "block mb-2 rounded-lg text-sm font-medium text-gray-900 dark:text-gray-400"
-                   type = 'time'
-                   placeholder="Pickup Time"
-                   name="pickupTime"
-                   id="pickupTime"
-            />
-          </div>
-          <div>
-            <input v-model = "returnTime"
-                   @input="getreturnTime"
-                   class = "block mb-2 rounded-lg text-sm font-medium text-gray-900 dark:text-gray-400"
-                   type = 'time'
-                   placeholder="Return Time"
-                   name="returnTime"
-                   id="returnTime"
-            />
-          </div>
-          <button @click.prevent="getSearch()" value="submit" class= "mt-6 w-1/2 items-center py-2  p-2.5 rounded-md border border-transparent bg-indigo-900 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 text-center"> Search </button>
+
+            <div class="flex flex-1 flex-col ">
+              <input
+                  v-model = "returnTime"
+                  @input="getreturnTime"
+                  class = "col-span-1 block mb-2 rounded-lg text-sm font-medium text-gray-900 dark:text-gray-400"
+                  type = 'time'
+                  placeholder="Return Time"
+                  name="returnTime"
+                  id="returnTime"
+              />
+              <label for="returnTime" class=" text-center lock text-sm font-medium text-gray-700">Return Time</label>
+            </div>
+
+          <div class="col-span-1">
+        <button @click.prevent="getSearch()" value="submit"
+
+                class= "block items-center py-2  p-2.5 rounded-md border border-transparent bg-indigo-900 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 text-center"> Search
+        </button>
+      </div>
         </div>
       </div>
     </form>
-  </div>
+    </div>
+
+
+  <div class="container w-90 lg:w-4/5 mx-auto flex flex-col">
 
   <div v-if="carSelection == null">
 
-    <ul role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-      <li v-for="(car, k, index) in cars"  class="relative">
+    <ul role="list" class="divide-y divide-gray-200">
+      <li v-for="(car, k, index ) in cars" class="flex flex-col md:flex-row overflow-hidden
+          bg-white rounded-lg shadow-xl h-50 mt-4 w-100 mx-2">
 
-        {{car.car_name}}
+<div class="p-3 rounded-md shadow-lg md:w-3/4">
+<img src="car.img"  height='30'
+     class=" inset-0 w-90 h-80 w-full object-cover object-center"/>
+</div>
+        <div class="w-full py-4 px-5 text-gray-800 flex flex-col justify-between">
 
-        <button type="button" class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" @click.prevent="openDetails(car)">Car Details </button>
+          <div class=" capitalize text-lg font-semibold text-gray-600">{{car.car_name }} or Similar </div>
+
+          <div class="flex items-center">
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="image2vector.svg"
+                 class="h-3 w-3 text-blue-500 fill-current mr-1" role="img" xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 384 512">
+              <path fill="currentColor"
+                    d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path>
+            </svg>
+            <div class="text-left text-md text-gray-600 font-semibold"> {{ location}}</div></div>
+
+          <div class="text-left text-md text-gray-600 font-semibold"> {{ car.entity_name }}</div>
+          <div class="text-left text-lg le.ading-tight text-gray-600 font-semibold"> Bags: {{ car.bags }}</div>
+          <div class="text-left text-lg text-gray-600 font-semibold"> Rental Agency: {{ car.vndr }}</div>
+
+
+
+          <div class="text-right text-xl leading-tight text-gray-600 font-semibold"> Price of Vehicle: ${{ car.price }}</div>
+
+
+        <button type="button"
+                class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" @click.prevent="openDetails(car)"> View Car Details  </button>
+        </div>
       </li>
     </ul>
   </div>
-
-  <div v-if="carSelection !== null">
-
-    <button type="button" class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" @click="closeDetails">close details</button>
-
-
-    <span v-for="image in carSelection.images"><img :src="image" alt="" class="pointer-events-none object-cover group-hover:opacity-75" width="300"/></span>
-
-
   </div>
+
+  <div v-if="carSelection !== null">>
+
+    <button type="button"
+            class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            @click="closeDetails"> close details
+    </button>
+
+</div>
   <footer class="mx-auto w-full  bg-white mt-auto" aria-labelledby="footer-heading">
     <div class="mx-auto  py-15 px-4 sm:px-6 lg:py-20 lg:px-8">
       <div class="mt-12 border-t border-gray-200 pt-8">
@@ -173,6 +220,7 @@ export default {
           pickUpTime: moment(this.pickupTime).format('HH:mm'),
           dropOffDate: moment(this.returnDate).format('YYYY-MM-DD'),
           dropOffTime: moment(this.returnTime).format('HH:mm'),
+          price:'num'
         },
         headers: {
           'X-RapidAPI-Key': '2756954a36mshd7f9836f6a3787bp186d1djsn79f785078e5b',
@@ -182,6 +230,7 @@ export default {
       axios.request(options).then(function (response) {
         console.log(response.data.data);
         self.cars = response.data.data.groups;
+        self.cars = response.data.data.quotes;
       }).catch(function (error) {
         console.error(error);
       });
