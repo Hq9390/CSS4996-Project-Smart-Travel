c99d2493dd14dde3eea91226e2da5ba9805a5056
bhohentanner <98716088+bhohentanner@users.noreply.github.com>
2022-12-05 12:23:25 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import FlightsView from '../views/FlightsView.vue'
+import RecommendedCitiesView from '../views/RecommendedCitiesView.vue'
+import CarRentalsView from '../views/CarRentalsView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import LoginView from '../views/LoginView.vue'
+import ProfileView from '../views/ProfileView.vue'
+import ForgotPasswordView from '../views/ForgotPasswordView.vue'
+import RatingView from '../views/RatingView.vue'
+import FlightRatingView from '../views/FlightRating.vue'
+import CarRentalRatingView from '../views/CarRentalRating.vue'
+import HotelRatingView from "../views/HotelRating.vue";
+import AdminPageView from "../views/AdminPage.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  linkActiveClass: 'active',
+  routes: [
+    {
+      path: '/',
+      name: 'hotels',
+      component: HomeView
+    },
+    {
+      path: '/flights',
+      name: 'flights',
+      component: FlightsView
+    },
+    {
+      path: '/recommendations',
+      name: 'recommendations',
+      component: RecommendedCitiesView
+    },
+
+    {
+      path: '/car-rentals',
+      name: 'car-rentals',
+      component: CarRentalsView
+    },
+    {
+      path: '/admin-page',
+      name: 'admin-page',
+      component: AdminPageView
+    },
+
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView
+    },
+    {
+      path: '/forgot-password',
+      name: 'forgot-password',
+      component: ForgotPasswordView
+    },
+    {
+      path: '/rating',
+      name: 'rating',
+      component: RatingView
+    },
+    {
+      path: '/flightRating',
+      name: 'flightRating',
+      component: FlightRatingView
+    },
+    {
+      path: '/carRating',
+      name: 'carRating',
+      component: CarRentalRatingView
+    },
+    {
+      path: '/hotelRating',
+      name: 'hotelRating',
+      component: HotelRatingView
+    }
+
+  ]
+})
+
+export default router
